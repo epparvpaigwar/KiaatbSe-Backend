b473f77a26928506c3cf100c6d157bd2130367c0
Parvpaigwar <parvpaigwar@gmail.com>
2025-05-24 21:20:46 +0530

    f

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include  # ✅ include is required
 
+def home(request):
+    return JsonResponse({"message": "KitaabSe Backend is running."})
+
 urlpatterns = [
+    path('', home),
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),  # ✅ users app URLs
 ]
