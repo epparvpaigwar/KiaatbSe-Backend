324ee7b2d6a892c2beb9d8b618bea6f66132595e
epparvpaigwar <parv@engineerphilosophy.com>
2025-11-09 01:34:19 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','kiaatbse-backend.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','kitaabse-backend.onrender.com']
 
 
 
